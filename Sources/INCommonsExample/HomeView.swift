65ea45c6ac66ba054b7094541570d8b3eaaa731f
Sven Korset <sven.korset@indie-software.com>
2022-11-05 07:16:18 +0100

    Added onFirstAppear view modifier

--- Sources/INCommonsExample/HomeView.swift
+++ Sources/INCommonsExample/HomeView.swift
@@ -11,8 +11,9 @@ struct FunctionalityRow: View {
 struct HomeView: View {
 	let exampleViews = [
 		ExampleViewFactory(title: "Configuration", view: { AnyView(ConfigurationView()) }),
-		ExampleViewFactory(title: "Share Sheet", view: { AnyView(ShareSheet()) }),
 		ExampleViewFactory(title: "Device Provider", view: { AnyView(DeviceProviderView()) }),
+		ExampleViewFactory(title: "First Appear View", view: { AnyView(FirstAppearView()) }),
+		ExampleViewFactory(title: "Share Sheet", view: { AnyView(ShareSheet()) }),
 		ExampleViewFactory(title: "View Condition", view: { AnyView(ViewConditions()) }),
 		ExampleViewFactory(title: "View Frame", view: { AnyView(ViewFrame()) })
 	]
