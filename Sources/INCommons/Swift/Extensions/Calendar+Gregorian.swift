11efd9abc7b21c49afdac5920947fb3a281e6751
Shota <sh.klibadze@gmail.com>
2024-04-13 12:36:43 +0400

    Removed locale from calendars.

--- Sources/INCommons/Swift/Extensions/Calendar+Gregorian.swift
+++ Sources/INCommons/Swift/Extensions/Calendar+Gregorian.swift
@@ -6,7 +6,18 @@ public extension Calendar {
 	/// The calendar's locale and timeZone are set to `testableCurrent` to prevent flaky tests.
 	static let gregorian: Calendar = {
 		var calendar = Calendar(identifier: .gregorian)
-		calendar.locale = .testableCurrent
+		// Not assigning locale to a calendar means the locale is nil.
+		// We are not assigning locale because it overrides calendar type.
+		// This means that if we want to display weekdays according to
+		// Gregorian calendar (week starting from Sunday)
+		// locale should not be assigned. If we assign locale .current
+		// and the user happens to be in a country that is not accustomed to
+		// week starting from Sunday the locale will override the Calendar type
+		// and it will act as iso8601 calendar (the start of the week will be Monday).
+		// Most countries are not used to a week starting from Sunday apart from
+		// US, Canada, Japan, Philipines, Brazil, and some other South American countries.
+		// And since we are not using locale to command a Calendar to provide its information
+		// in different languages, the locale in our case can be Nil.
 		calendar.timeZone = .testableCurrent
 		return calendar
 	}()
