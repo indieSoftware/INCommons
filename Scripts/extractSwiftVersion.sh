b8e28ade4464215c89bbc96cceddbbf892f46c69
Hüseyin <huseyinbagana@yandex.com>
2024-04-21 12:07:45 +0200

    Implement overlay button

--- Scripts/extractSwiftVersion.sh
+++ Scripts/extractSwiftVersion.sh
@@ -2,4 +2,4 @@
 # Writes the current swift version into a file.
 # Intended to be run by Xcode to extract the swift version used by SwiftFormat.
 
-swift -version | grep -Eo 'Swift version [0-9]+(\.[0-9])*' | grep -Eo '[0-9]+(\.[0-9])*' | tee .swift-version
+swift -version | grep -Eo 'Swift version [0-9]+(\.[0-9]+)*' | grep -Eo '[0-9]+(\.[0-9]+)*' | tee .swift-version
